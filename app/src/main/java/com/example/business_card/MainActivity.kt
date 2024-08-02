916cbefed7a5bfb5ce5bbbdad8496f365ac4c3dc
JoJaCo <contrerasjorge226@gmail.com>
2024-08-02 01:25:06 -0700

    Finsied our first compose app

--- app/src/main/java/com/example/business_card/MainActivity.kt
+++ app/src/main/java/com/example/business_card/MainActivity.kt
@@ -6,9 +6,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -40,9 +42,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.textInputServiceFactory
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -120,7 +125,7 @@ fun CreateBizCard(){
     }
 
 }
-//
+//data of the clients
 @Preview
 @Composable
 fun Content(){
@@ -143,12 +148,32 @@ fun Content(){
     }
 
 }
-
+//Column of the data
 @Composable
 fun Portfolio(data: List<String>) {
     LazyColumn{items(data){ item ->
-    Text(text = item)
-    } }
+        Card(modifier = Modifier
+            .padding(13.dp)
+            .fillMaxWidth(),
+            shape = RectangleShape,
+            elevation = CardDefaults.cardElevation(4.dp),){
+            Row(modifier = Modifier
+                .padding(8.dp)
+                .background(MaterialTheme.colorScheme.surface)
+                .padding(7.dp)){
+                CreateImageProfile(modifier = Modifier.size(100.dp))
+                Column(modifier = Modifier
+                    .padding(7.dp)
+                    .align(alignment = Alignment.CenterVertically)) {
+
+                    Text(text = item, fontWeight = FontWeight.Bold)
+                    Text(text = "A great Project", style = MaterialTheme.typography.bodySmall)
+                }
+            }
+
+            }
+        }
+    }
 }
 
 
@@ -166,7 +191,7 @@ private fun ClientInfo() {
 @Composable
 private fun CreateImageProfile(modifier: Modifier = Modifier) {
     Surface(
-        modifier = Modifier
+        modifier = modifier
             .size(150.dp)
             .padding(5.dp),
         shape = CircleShape,
@@ -177,7 +202,8 @@ private fun CreateImageProfile(modifier: Modifier = Modifier) {
         Image(
             painter = painterResource(id = R.drawable.profile_image),
             contentDescription = "user picture",
-            modifier = Modifier.size(135.dp)
+            modifier = modifier.size(135.dp),
+            contentScale = ContentScale.Crop
         )
 
     }
